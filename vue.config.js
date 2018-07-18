b64aa81504b9830824a38c8d84a6f21c1acec2dc
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-07-17 22:55:36 -0500

    remove hardcoded path to .ejs

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].template =
-        "!!raw-loader!/home/anirbanmu/code/muc/views/index.ejs";
+        "!!raw-loader!./views/index.ejs";
       args[0].filename = "templates/index.ejs";
       return args;
     });
