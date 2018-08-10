debc32a2200cbea4f208d843df8d705074787f0b
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-08-09 20:31:23 -0500

    lint fix

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,9 @@ module.exports = {
       args[0].template = "!!raw-loader!./views/index.ejs";
       args[0].filename = "templates/index.ejs";
       if (process.env.GOOGLE_SITE_VERIFICATION_CODE) {
-        args[0].meta = { "google-site-verification": process.env.GOOGLE_SITE_VERIFICATION_CODE }
+        args[0].meta = {
+          "google-site-verification": process.env.GOOGLE_SITE_VERIFICATION_CODE
+        };
       }
       return args;
     });
