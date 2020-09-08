3417524f0a61f78effe3674edfb641dcc76dd117
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2020-09-07 23:56:38 -0700

    switch to fontawesome package instead of pulling vanilla js from cdn

--- src/components/SpotifyResultListItem.vue
+++ src/components/SpotifyResultListItem.vue
@@ -2,7 +2,9 @@
   <div class="level-item has-text-centered">
     <div>
       <div>
-        <a :href="link" target="_blank"> <i class="fab fa-spotify fa-5x" /> </a>
+        <a :href="link" target="_blank">
+          <font-awesome-icon :icon="['fab', 'spotify']" size="5x" />
+        </a>
       </div>
 
       <ResultListItemButtonBar :link="link" />
--- src/components/YoutubeResultListItem.vue
+++ src/components/YoutubeResultListItem.vue
@@ -2,7 +2,9 @@
   <div class="level-item has-text-centered">
     <div>
       <div>
-        <a :href="link" target="_blank"> <i class="fab fa-youtube fa-5x" /> </a>
+        <a :href="link" target="_blank">
+          <font-awesome-icon :icon="['fab', 'youtube']" size="5x" />
+        </a>
       </div>
 
       <ResultListItemButtonBar :link="link" />
