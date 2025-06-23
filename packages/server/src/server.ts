9a49b37f675f6889acc7d3ca792fc3a575e47e14
Anirban Mukhopadhyay <anirbanmu@users.noreply.github.com>
2025-06-22 23:02:31 -0700

    Created server package & proved that it can consume from common. Changed all module & moduleResolution to nodenext (all ESM)

---
+++ packages/server/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { sharedFunction } from "@muc/common";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const clientDistPath = path.resolve(import.meta.dirname, "../../client/dist");
+app.use(express.static(clientDistPath));
+
+app.get("/api/message", (req, res) => {
+  const message = sharedFunction();
+  res.json({ message });
+});
+
+app.get("", (req, res) => {
+  res.sendFile(path.resolve(clientDistPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
+  console.log(`Serving client assets from: ${clientDistPath}`);
+});
